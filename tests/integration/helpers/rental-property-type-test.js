6bfe6921eada7f12aaad406b755c556deaa99a74
Frankie Hayward <frankie@catalystsquared.com>
2017-06-07 15:01:17 -0400

    add a real test for rental-proprty-type
    this step was missing from the tutorial and through a fail during testing

--- tests/integration/helpers/rental-property-type-test.js
+++ tests/integration/helpers/rental-property-type-test.js
@@ -6,12 +6,10 @@ moduleForComponent('rental-property-type', 'helper:rental-property-type', {
   integration: true
 });
 
-// Replace this with your real tests.
 test('it renders', function(assert) {
-  this.set('inputValue', '1234');
+  this.set('inputValue', 'Condo');
 
   this.render(hbs`{{rental-property-type inputValue}}`);
 
-  assert.equal(this.$().text().trim(), '1234');
+  assert.equal(this.$().text().trim(), 'Community');
 });
-
